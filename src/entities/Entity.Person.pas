1983fe6f19a270b430fd74b634cb8718c844b690
Huelbert Rodrigo <rodrigo@scontti.com.br>
2022-07-29 15:04:56 -0300

    alter entity person

--- src/entities/Entity.Person.pas
+++ src/entities/Entity.Person.pas
@@ -17,40 +17,25 @@ uses
 type
   [Entity]
   [Table('person','')]
-  [PrimaryKey('person_id', AutoInc, SequenceInc, NoSort, True)]
+  [PrimaryKey('id', AutoInc, SequenceInc, NoSort, True)]
   [Indexe('idx_name','name')]
-  [OrderBy('person_id desc')]
+  [OrderBy('id desc')]
   [Sequence('person')]
   TPerson = class
   private
     { Private declarations }
     FId: Integer;
     FName: String;
-//    FBirthDate: TDate;
-//    FPhoto: TBlob;
-//    FActive: Boolean;
   public
     { Public declarations }
     [Restrictions([NoUpdate, NotNull])]
-    [Column('person_id', ftInteger)]
+    [Column('id', ftInteger)]
     [Dictionary('Id','validation message','','','',taCenter)]
-    property Id: Integer read FId write FId;
+    property id: Integer read FId write FId;
 
     [Column('name', ftString, 60)]
     [Dictionary('Name','validation message','','','',taLeftJustify)]
-    property Name: String read FName write FName;
-
-//    [Column('birth_date', ftDate)]
-//    [Dictionary('Birth Date','validation message')]
-//    property BirthDate: TDate read FBirthDate write FBirthDate;
-//
-//    [Column('photo', ftBlob)]
-//    [Dictionary('Photo','validation message')]
-//    property Photo: TBlob read FPhoto write FPhoto;
-//
-//    [Column('active', ftBoolean)]
-//    [Dictionary('Active','validation message')]
-//    property Active: Boolean read FActive write FActive;
+    property name: String read FName write FName;
   end;
 
 implementation
