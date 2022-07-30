e2eda70836586a478bd7fdda606e54d6d269235f
Huelbert Rodrigo <rodrigo@scontti.com.br>
2022-07-30 19:18:37 -0300

    add uses of new package

--- src/server/server.dpr
+++ src/server/server.dpr
@@ -7,30 +7,25 @@ program server;
 uses
   System.SysUtils,
   Horse,
-
+//  Horse.Jhonson,
+  Horse.Compression,
   dbebr.factory.interfaces,
   dbebr.factory.zeos,
-
   dbcbr.database.interfaces,
   ormbr.modeldb.compare,
-
   dbcbr.ddl.generator.firebird,
   dbcbr.metadata.firebird,
   ormbr.dml.generator.firebird,
-
   dbcbr.ddl.generator.sqlite,
   dbcbr.metadata.sqlite,
   ormbr.dml.generator.sqlite,
-
   dbcbr.ddl.generator.postgresql,
   dbcbr.metadata.postgresql,
   ormbr.dml.generator.postgresql,
-
-  ormbr.form.monitor,
-  ormbr.server.horse,
-
   uDMConnection in 'database\uDMConnection.pas' {DMConnection: TDataModule},
-  Entity.Person in '..\entities\Entity.Person.pas';
+  Entity.Person in '..\entities\Entity.Person.pas',
+  Controller.Person in 'controllers\Controller.Person.pas',
+  Repository.Person in 'repositories\Repository.Person.pas';
 
 procedure GetHome(Req: THorseRequest; Res: THorseResponse; Next: TNextProc);
 begin
@@ -39,24 +34,20 @@ end;
 
 procedure GetListen(Horse: THorse);
 var
-  oConnection: IDBConnection;
-  oManager: IDatabaseCompare;
-
-  dbDriver: TDriverName;
-
-  FRESTServerHorse: TRESTServerHorse;
+  FConnection: IDBConnection;
+  FManager: IDatabaseCompare;
 begin
-  dbDriver := dnFirebird;
+  try
+    DMConnection := TDMConnection.Create(nil);
 
-  DMConnection := TDMConnection.Create(nil);
-  DMConnection.dbDriver := dbDriver;
+    FConnection := TFactoryZeos.Create(DMConnection.ZConnection, DMConnection.dbDriver);
 
-  oConnection := TFactoryZeos.Create(DMConnection.ZConnection, dbDriver);
-  oManager := TModelDbCompare.Create(oConnection);
-  oManager.CommandsAutoExecute := True;
-  oManager.BuildDatabase;
-
-  FRESTServerHorse := TRESTServerHorse.Create(oConnection, 'api/v1');
+    FManager := TModelDbCompare.Create(FConnection);
+    FManager.CommandsAutoExecute := True;
+    FManager.BuildDatabase;
+  finally
+    DMConnection.Free;
+  end;
 
   Writeln(Format('Server is runing on %s:%d', [Horse.Host, Horse.Port]));
   Readln;
@@ -68,8 +59,13 @@ begin
     ReportMemoryLeaksOnShutdown := True;
   {$ENDIF}
 
+  THorse
+//    .Use(Jhonson)
+    .Use(Compression());
 
   THorse.Get('/', GetHome);
+
+  Controller.Person.Registry;
 
   THorse.Listen(5000, GetListen);
 end.
